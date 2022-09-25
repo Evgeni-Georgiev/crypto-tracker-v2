cab2234ee22c73a680a4a721597f7859aac1dcab
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-09-25 15:17:34 +0300

    Icons changed.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
-import {FaCoins} from "react-icons/fa";
-import "./Header.scss";
 import {Link} from "react-router-dom";
+import {FaBitcoin} from "react-icons/fa";
+import {BiTrendingUp} from "react-icons/bi";
+import "./Header.scss";
 
 export function Header() {
     return (
@@ -8,18 +9,18 @@ export function Header() {
 
         <Link to="/">
             <div className="navbar">
-                <FaCoins className="icon"/>
+                {/*<FaCoins className="icon"/>*/}
+                <FaBitcoin className="icon"/>
                 <h2>Coin <span className="purple">Search</span> </h2>
             </div>
         </Link>
 
             <Link to="/trending">
                 <div className="navbar">
-                    <FaCoins className="icon"/>
+                    <BiTrendingUp className="icon"/>
                     <h2>Trending <span className="purple">Coins</span> </h2>
                 </div>
             </Link>
-
         </div>
     );
 }
